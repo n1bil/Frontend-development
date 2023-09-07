0e4d64ac21f4185edf2beb6d30a54f7d967b9f95
nabil <nabil.nadzor@gmail.com>
2023-09-07 11:43:12 +0200

    Lesson 12

--- lesson12_DOM_06_09_2023/homework/script.js
+++ lesson12_DOM_06_09_2023/homework/script.js
@@ -6,15 +6,26 @@ addTaskButton.addEventListener('click', addTask);
 
 taskListUl.addEventListener('click', (e) => {
     console.log(e);
-    const spanText = e.target.previousSibling;
-    spanText.classList.add('strikeThrough');
+    // const spanText = e.target.previousSibling;
+    // spanText.classList.add('strikeThrough');
+    if (e.target.tagName === 'BUTTON') {            // if we clicked the button
+        // console.log('We clicked the button');       // output log
+        e.target.parentElement.remove();
+    }
+    if (e.target.checked) {     // if checked = true
+        e.target.previousElementSibling.style.textDecoration = 'line-through';
+    } else {
+        e.target.previousElementSibling.style.textDecoration = 'none';
+    }
 })
 
 function addTask() {
     const taskName = task.value.trim();
     if (taskName) {
         const li = document.createElement('li');
-        li.innerHTML = `<span>${taskName}</span><button>Delete</button>`;
+        li.innerHTML = `<span>${taskName}</span>
+                        <input type='checkbox'>
+                        <button>Delete</button>`;
         taskListUl.appendChild(li);
     }
     task.value = '';
