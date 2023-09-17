89830134a51120767fb64bdfbd8f7345de3e7a72
nabil <nabil.nadzor@gmail.com>
2023-09-17 22:19:02 +0200

    Lesson 17

---
+++ lesson17_homework/src/components/ToDo.js
@@ -0,0 +1,28 @@
+const task = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTask');
+const taskListUl = document.getElementById('taskList');
+
+addTaskButton.addEventListener('click', addTask);
+
+taskListUl.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        e.target.parentElement.remove();
+    }
+    if (e.target.checked) {
+        e.target.previousElementSibling.style.textDecoration = 'line-through';
+    } else {
+        e.target.previousElementSibling.style.textDecoration = 'none';
+    }
+})
+
+function addTask() {
+    const taskName = task.value.trim();
+    if (taskName) {
+        const li = document.createElement('li');
+        li.innerHTML = `<span>${taskName}</span>
+                        <input type='checkbox'>
+                        <button>Delete</button>`;
+        taskListUl.appendChild(li);
+    }
+    task.value = '';
+}
