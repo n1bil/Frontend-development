dd3560202bee63c031fc73d12584a2dcf7b51f85
nabil <nabil.nadzor@gmail.com>
2023-09-19 10:25:40 +0200

    Lesson 17

---
+++ lesson17_react_ts_15_09_2023/src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import React, { FC, useState } from 'react'
+
+const Counter: FC = (): JSX.Element => {
+    const [count, setCount] = useState<number>(0);
+
+    const increment = (): void => { setCount(count + 1) }
+
+  return (
+    <>
+    <h1>Counter: {count}</h1>
+    <button onClick={increment}>Increment</button>
+    <button onClick={(): void => setCount(count - 1)}>Decrement</button>
+    </>
+  )
+}
+
+export default Counter
