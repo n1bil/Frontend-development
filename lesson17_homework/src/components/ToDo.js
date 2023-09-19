99a29986aa1fd24ee2a2f706ee28f405a6313fb7
nabil <nabil.nadzor@gmail.com>
2023-09-19 21:04:49 +0200

    Lesson 17

--- lesson17_homework/src/components/ToDo.js
+++ lesson17_homework/src/components/ToDo.js
@@ -1,3 +1,60 @@
+import React, { useState } from "react";
+
+function ToDo() {
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        const trimmedTask = task.trim();
+        if (trimmedTask) {
+            setTasks([...tasks, { name: trimmedTask, completed: false }]);
+            setTask("");
+        }
+    };
+
+    const deleteTask = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    }
+
+    const handleIsDone = (e, taskIndex) => {
+        const updatedTasks = [...tasks];
+        updatedTasks[taskIndex].completed = e.target.checked;
+        setTasks(updatedTasks);
+    }
+
+    return (
+        <div className="container">
+            <h1>TODO List App</h1>
+            <div className="input-container">
+                <input
+                    type="text"
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                    placeholder="Enter a task..."
+                />
+                <button id="addTask" onClick={addTask}>Add</button>
+            </div>
+            <ol>
+                {tasks.map((task, index) => (
+                    <div key={index} className="task">
+                        <li style={task.completed ? {textDecoration: 'line-through'} : {}}>
+                            <span>{index + 1}. </span>
+                            {task.name}
+                            <input type="checkbox" id="isDone" onChange={(e) => handleIsDone(e, index)} checked={task.completed} />
+                            <button onClick={() => deleteTask(index)}>Delete</button>
+                        </li>
+                    </div>
+                ))}
+            </ol>
+        </div>
+    );
+}
+
+export default ToDo;
+
+/*
 const task = document.getElementById('taskInput');
 const addTaskButton = document.getElementById('addTask');
 const taskListUl = document.getElementById('taskList');
@@ -26,3 +83,4 @@ function addTask() {
     }
     task.value = '';
 }
+*/
